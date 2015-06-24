f11f414ab8760b37d5f805cf16f313292ef2d179
Sander M <S.N.Maijers@gmail.com>
2015-06-24 16:03:21 +0200

    * Try to fix service/servicegroup declarations for UU, as their services do not show up in Icinga.

--- probes/check_clarin_saml.sh
+++ probes/check_clarin_saml.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
 clean_up() {
-  rm -f "{$XML_file}" ; 
+  rm -f "{$XML_file}" ;
   rm -f "{$signing_certificate_file}"
 }
 
 # TODO: this scripts needs to be fixed and integrated in probe_curl.sh
 
-XML_file="$(mktemp --suffix='check_clarin__saml_XML_file')" && 
-signing_certificate_file="$(mktemp --suffix='check_clarin_saml__signing_certificate')" && 
-curl --tlsv1 -f -L -s -v -o "${signing_certificate_file}" 'https://www.clarin.eu/sites/default/files/SPF_signing_pub_1.crt' 1>&2 && 
-curl --tlsv1.2 -f -L -s -v -o "${XML_file}" -w "@${2}/curl.format" "https://$1/aai/prod_md_about_spf_sps.xml" 1>&2 && 
+XML_file="$(mktemp --suffix='check_clarin__saml_XML_file')" &&
+signing_certificate_file="$(mktemp --suffix='check_clarin_saml__signing_certificate')" &&
+curl --tlsv1 -f -L -s -v -o "${signing_certificate_file}" 'https://www.clarin.eu/sites/default/files/SPF_signing_pub_1.crt' 1>&2 &&
+curl --tlsv1.2 -f -L -s -v -o "${XML_file}" -w "@${2}/curl.format" "https://$1/aai/prod_md_about_spf_sps.xml" 1>&2 &&
 sh 'xmlsectool.sh' --inFile "${XML_file}" --validateSchema --schemaDirectory 'saml-schema/' --verifySignature --certificate "${signing_certificate_file}" 1>&2 || exit 2
 
 trap 'clean_up' EXIT
