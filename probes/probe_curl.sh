1f47e1a4806d670a651798397a2a76aa73af094a
Sander M <S.N.Maijers@gmail.com>
2015-06-24 13:58:42 +0200

    * Redirect all command output on stderr to stdout, so that Icinga will actually show it in service status details.

--- probes/probe_curl.sh
+++ probes/probe_curl.sh
@@ -8,7 +8,7 @@ probe_curl_head() {
     # $1: Full URL.
     # $2: Root directory path for curl.format and other data files.
     # $3: Optional extra command-line parameter(s) for curl.
-    /usr/bin/curl --fail --head --location --referer "${REFERER}" --show-error --silent --tlsv1.2 --verbose --write-out $3 "@${2}/curl.format" "${1}" ||
+    /usr/bin/curl --fail --head --location --referer "${REFERER}" --show-error --silent --tlsv1.2 --verbose --write-out "@${2}/curl.format" "${1}" $3 2>&1 ||
     return "$?" ;
 }
 
@@ -18,7 +18,7 @@ probe_curl_get() {
     # $2: Root directory path for curl.format and other data files.
     # $3: Expected Internet media type of HTTP response (MIME/content type).
     # $4: Optional extra command-line parameter(s) for curl.
-    /usr/bin/curl --fail --header "Accept: ${3}" --location --output "${tmp_file_path}" --referer "${REFERER}" --show-error --silent --tlsv1.2 --verbose --write-out "@${2}/curl.format" "${1}" $4 1>&2 &&
+    /usr/bin/curl --fail --header "Accept: ${3}" --location --output "${tmp_file_path}" --referer "${REFERER}" --show-error --silent --tlsv1.2 --verbose --write-out "@${2}/curl.format" "${1}" $4 2>&1 &&
     cat "${tmp_file_path}" ||
     return "$?" ;
 }
@@ -34,7 +34,7 @@ probe_curl_imdi() {
     # $2: Root directory path for curl.format and other data files.
     # $3: Optional extra command-line parameter(s) for curl.
     probe_curl_get "${1}" "${2}" 'application/xml' $3 |
-    xmllint --noout - || # TODO: validate using correct schema "$2/IMDI/IMDI_3.0.xsd"
+    xmllint --noout - 2>&1 || # TODO: validate using correct schema "$2/IMDI/IMDI_3.0.xsd"
     return "$?" ;
 }
 
@@ -44,7 +44,7 @@ probe_curl_handle_json() {
     # $2: Root directory path for curl.format and other data files.
     # $3: Optional extra command-line parameter(s) for curl.
     probe_curl_get "${1}" "${2}" 'application/xml' $3 |
-    python -I -c 'from json import load; from sys import stdin, exit; json_obj=load(stdin); exit(0 if json_obj["responseCode"] == 1 else 2)' ||
+    python -I -c 'from json import load; from sys import stdin, exit; json_obj=load(stdin); exit(0 if json_obj["responseCode"] == 1 else 2)' 2>&1 ||
     return "$?" ;
 }
 
@@ -54,7 +54,7 @@ probe_sru_endpoint() {
     # $2: Root directory path for curl.format and other data files.
     # $3: Optional extra command-line parameter(s) for curl.
     probe_curl_get "${1}" "${2}" 'application/xml' $3 |
-    xmllint --noout - || # TODO: validate using definite set of schemas --schema "$2/SRU.xsd"
+    xmllint --noout - 2>&1 || # TODO: validate using definite set of schemas --schema "$2/SRU.xsd"
     return "$?" ;
 }
 
@@ -64,9 +64,9 @@ probe_oai_pmh_endpoint() {
     # $2: Root directory path for curl.format and other data files.
     # $3: Optional extra command-line parameter(s) for curl.
     { probe_curl_get "${1}" "${2}" 'application/xml' $3 |
-    xmllint --noout --schema "$2/OAI-PMH/OAI-PMH.xsd" - ; } ||
+    xmllint --noout --schema "$2/OAI-PMH/OAI-PMH.xsd" - ; } 2>&1 ||
     probe_curl_head "${1}" |
-    grep -Eio 'Retry-After: [[:digit:]]+' ||
+    grep -Eio 'Retry-After: [[:digit:]]+' 2>&1 ||
     return "$?" ;
 }
 
@@ -86,7 +86,7 @@ probe_saml_metadata() {
     # $2: Root directory path for curl.format and other data files.
     # $3: Optional extra command-line parameter(s) for curl.
     probe_curl_get "${1}" "${2}" 'application/xml' $3 |
-    xmllint --noout - 1> '/dev/null' || # TODO: validate using SAML tools
+    xmllint --noout - 1> '/dev/null' 2>&1 || # TODO: validate using SAML tools
     return "$?" ;
 }
 
