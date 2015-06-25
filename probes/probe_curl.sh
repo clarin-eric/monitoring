5aca9ba5d0f7247ac120ec59a86660da02806c7d
Sander M <S.N.Maijers@gmail.com>
2015-06-25 12:15:49 +0200

    * Validate IMDI after Handle resolution, now that the invalid record the test Handle resolved to has been corrected. * Make probe naming more consistent and separate private from public functions more clearly. * Simplify and fix probe_curl.sh.

--- probes/probe_curl.sh
+++ probes/probe_curl.sh
@@ -5,55 +5,70 @@ tmp_details_file_path="$(mktemp)"
 REFERER='https://clarin.fz-juelich.de/icinga/'
 
 saved_traps=$(trap)
-trap 'rm -vf "${temp_data_file_path}" ; $saved_traps' 'EXIT' 'INT' 'TERM' 'HUP'
+trap 'rm -vf "${temp_data_file_path}" ; trap "${saved_traps}"' 'EXIT' 'INT' 'TERM' 'HUP'
 
-probe_curl_head() {
+##
+## Private functions
+##
+
+_probe_curl_http_head() {
     # HTTP HEAD request.
     # $1: Full URL.
     # $2: Root directory path for curl.format and other data files.
     # $3: Optional extra command-line parameter(s) for curl.
-    performance_data_1=$(/usr/bin/curl --fail --head --location --referer "${REFERER}" --show-error --silent --stderr "${tmp_details_file_path}" --tlsv1.2 --verbose --write-out "@${2}/curl.format" "${1}" $3 ; ) ;
+    performance_data_1=$(/usr/bin/curl --fail --head --location --referer "${REFERER}" --show-error --silent --stderr "${tmp_details_file_path}" --tlsv1.2 --verbose --write-out "@${2}/curl.format" $3 "${1}" ; ) ;
     curl_exit_status="$?" ;
-    details_data="$(cat ${tmp_details_file_path})" ;
-    printf 'exit status: %d | %s\n%s %s\n\n' "${curl_exit_status}" "${performance_data_1}" "${details_data}"
+    details_data="$(cat "${tmp_details_file_path}" ; )" ;
+    printf 'curl exit status: %d | %s\n%s %s\n\n' "${curl_exit_status}" "${performance_data_1}" "${details_data}" ;
     return "${curl_exit_status}" ;
 }
 
-probe_curl_get() {
+_probe_curl_http_get() {
     # HTTP GET request.
     # $1: Full URL.
     # $2: Root directory path for curl.format and other data files.
     # $3: Expected Internet media type of HTTP response (MIME/content type).
     # $4: Optional extra command-line parameter(s) for curl.
-    performance_data_1=$(/usr/bin/curl --fail --header "Accept: ${3}" --location --output "${temp_data_file_path}" --referer "${REFERER}" --show-error --silent --stderr "${tmp_details_file_path}" --tlsv1.2 --verbose --write-out "@${2}/curl.format" "${1}" $4 ; ) ;
+    performance_data_1=$(/usr/bin/curl --fail --header "Accept: ${3}" --location --output "${temp_data_file_path}" --referer "${REFERER}" --show-error --silent --stderr "${tmp_details_file_path}" --tlsv1.2 --verbose --write-out "@${2}/curl.format" $4 "${1}" ; ) ;
     curl_exit_status="$?" ;
-    details_data="$(cat ${tmp_details_file_path})" ;
-    printf 'exit status: %d | %s\n%s %s\n\n' "${curl_exit_status}" "${performance_data_1}" "${details_data}" &&
+    details_data=$(cat "${tmp_details_file_path}" ; ) ;
+    printf 'curl exit status: %d | %s\n%s %s\n\n' "${curl_exit_status}" "${performance_data_1}" "${details_data}" ;
     return "${curl_exit_status}" ;
 }
 
-probe_curl_ldap() {
-    /usr/bin/curl --fail --netrc --show-error --silent --stderr - --verbose --write-out "@${2}/curl.format" "${1}" $2 ||
-    # TODO: bring in agreement with performance_data_1/details_data breakdown of other curl probes.
-    return 2 ;
+_probe_curl_ldap() {
+    # LDAP bind.
+    # $1: Full URL.
+    # $2: Root directory path for curl.format and other data files.
+    # $3: Optional extra command-line parameter(s) for curl.
+    performance_data_1=$(/usr/bin/curl --fail --netrc --output "${temp_data_file_path}" --show-error --silent --stderr "${tmp_details_file_path}" --tlsv1 --verbose --write-out "@${2}/curl.format" $2 "${1}" ; ) ;
+    curl_exit_status="$?" ;
+    details_data=$(cat "${tmp_details_file_path}" ; ) ;
+    printf 'curl exit status: %d | %s\n%s %s\n\n' "${curl_exit_status}" "${performance_data_1}" "${details_data}" ;
+    return "${curl_exit_status}" ;
 }
 
-probe_curl_imdi() {
+##
+## Public data-validating functions
+##
+
+probe_imdi() {
     # HTTP GET request with IMDI response data validation.
     # $1: Full URL.
     # $2: Root directory path for curl.format and other data files.
     # $3: Optional extra command-line parameter(s) for curl.
-    probe_curl_get "${1}" "${2}" 'application/xml' $3 &&
-    xmllint --noout <"${temp_data_file_path}" 2>&1 || # TODO: validate using correct schema "$2/IMDI/IMDI_3.0.xsd"
+    _probe_curl_http_get "${1}" "${2}" 'application/xml' $3 &&
+    xmllint --noout --schema "$2/IMDI/IMDI_3.0.xsd" "${temp_data_file_path}" 2>&1 ||
+    # TODO: validate using correct schema "$2/IMDI/IMDI_3.0.xsd"
     return 2 ;
 }
 
-probe_curl_handle_json() {
+probe_handle_json() {
     # HTTP GET request with Handle JSON response data validation.
     # $1: Full URL.
     # $2: Root directory path for curl.format and other data files.
     # $3: Optional extra command-line parameter(s) for curl.
-    probe_curl_get "${1}" "${2}" 'application/xml' $3 &&
+    _probe_curl_http_get "${1}" "${2}" 'application/xml' $3 &&
     python3 -I -c 'from json import load; from sys import stdin, exit; json_obj=load(stdin); exit(0 if json_obj["responseCode"] == 1 else 2)' <"${temp_data_file_path}" 2>&1 ||
     return 2 ;
 }
@@ -63,9 +78,8 @@ probe_sru_endpoint() {
     # $1: Full URL.
     # $2: Root directory path for curl.format and other data files.
     # $3: Optional extra command-line parameter(s) for curl.
-    probe_curl_get "${1}" "${2}" 'application/xml' $3 ||
+    _probe_curl_http_get "${1}" "${2}" 'application/xml' $3 ||
     # TODO: validate using definite set of schemas --schema "$2/SRU.xsd"
-    #xmllint --noout "${temp_data_file_path}" 2>&1 ||
     return 2 ;
 }
 
@@ -74,22 +88,16 @@ probe_oai_pmh_endpoint() {
     # $1: Full URL.
     # $2: Root directory path for curl.format and other data files.
     # $3: Optional extra command-line parameter(s) for curl.
-    probe_curl_get "${1}?verb=Identify" "${2}" 'application/xml' $3 ;
+    _probe_curl_http_get "${1}?verb=Identify" "${2}" 'application/xml' $3 ;
     curl_exit_status="$?"
     if [ $curl_exit_status -eq 22 ]; then
         # Check for a probable 503 response that gives a Retry-After response header: we accept this as it is.
         probe_curl_head "${1}?verb=Identify" "${2}" $3 |
-        grep -Eio 'Retry-After: [[:digit:]]+' 2>&1 ;
-        grep_exit_status="$?"
-        if [ $grep_exit_status -ne 0 ]; then
-            return 2 ;
-        fi
+        grep -Eio 'Retry-After: [[:digit:]]+' 2>&1 ||
+        return 2 ;
     else
-        xmllint --noout --schema "$2/_OAI-PMH-all.xsd" "${temp_data_file_path}" 2>&1
-        xmllint_exit_status="$?"
-        if [ $xmllint_exit_status -ne 0 ]; then
-            return 2 ;
-        fi
+        xmllint --noout --schema "$2/_OAI-PMH-all.xsd" "${temp_data_file_path}" 2>&1 ||
+        return 2 ;
     fi
 }
 
@@ -98,7 +106,7 @@ probe_json() {
     # $1: Full URL.
     # $2: Root directory path for curl.format and other data files.
     # $3: Optional extra command-line parameter(s) for curl.
-    probe_curl_get "${1}" "${2}" 'application/json' $3 &&
+    _probe_curl_http_get "${1}" "${2}" 'application/json' $3 &&
     python3 -I -m 'json.tool' <"${temp_data_file_path}" 1> '/dev/null' 2>&1 ||
     return 2 ;
 }
@@ -108,8 +116,40 @@ probe_saml_metadata() {
     # $1: Full URL.
     # $2: Root directory path for curl.format and other data files.
     # $3: Optional extra command-line parameter(s) for curl.
-    probe_curl_get "${1}" "${2}" 'application/xml' $3 &&
+    _probe_curl_http_get "${1}" "${2}" 'application/xml' $3 &&
     # TODO: validate using SAML tools
-    xmllint --noout - 1> '/dev/null' "${temp_data_file_path}" 2>&1 ||
+    xmllint --noout "${temp_data_file_path}" 2>&1 ||
+    return 2 ;
+}
+
+##
+## Public functions (wrapped for Icinga)
+##
+
+probe_http_head() {
+    # HTTP HEAD request.
+    # $1: Full URL.
+    # $2: Root directory path for curl.format and other data files.
+    # $3: Optional extra command-line parameter(s) for curl.
+    _probe_curl_http_head "${1}" "${2}" $3 ||
+    return 2 ;
+}
+
+probe_http_get() {
+    # HTTP GET request.
+    # $1: Full URL.
+    # $2: Root directory path for curl.format and other data files.
+    # $3: Expected Internet media type of HTTP response (MIME/content type).
+    # $4: Optional extra command-line parameter(s) for curl.
+    _probe_curl_http_get "${1}" "${2}" $3 ||
+    return 2 ;
+}
+
+probe_ldap_bind() {
+    # LDAP bind.
+    # $1: Full URL.
+    # $2: Root directory path for curl.format and other data files.
+    # $3: Optional extra command-line parameter(s) for curl.
+    _probe_curl_ldap_bind "${1}" "${2}" $3 ||
     return 2 ;
 }
